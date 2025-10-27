68382871b68ae481f3ba59acdae73e1778ff82ab
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-10-27 11:37:55 +0100

    WIP Solve Challenge04

---
+++ src/Challenge04.kt
@@ -0,0 +1,76 @@
+import kotlin.io.path.readLines
+
+fun main() {
+    fun mySolution(input: List<String>): String {
+        val keyPad = listOf(
+            listOf('A', 'B', 'C', 'D'),
+            listOf('E', 'F', 'G', 'H'),
+            listOf('I', 'J', 'K', 'L'),
+            listOf('M', 'N', 'O', 'P'),
+        )
+
+        val instructions = input.map { line ->
+            line.split(',')
+                .map { it.trim() }
+                .map { direction ->
+                    direction.toDirectionWithDeltas()
+                }
+        }
+
+        instructions.println()
+
+        fun areCoordinatesNotOutOfBounds(
+            newI: Int,
+            newJ: Int
+        ) = newI in keyPad.indices && newJ in keyPad.first().indices
+
+        return buildString {
+            instructions.forEach { instruction ->
+                val currentCoordinate = Coordinate(0, 0)
+                instruction.forEach { direction ->
+                    val newI = currentCoordinate.i + direction.iDelta
+                    val newJ = currentCoordinate.j + direction.jDelta
+                    if (areCoordinatesNotOutOfBounds(newI, newJ)) {
+                        currentCoordinate.i = newI
+                        currentCoordinate.j = newJ
+                    }
+                }
+                append(keyPad[currentCoordinate.i][currentCoordinate.j])
+            }
+        }
+    }
+
+    val input = pathToInput("Challenge04")
+        .readLines()
+
+    val inputTest = pathToInput("Challenge04Test")
+        .readLines()
+
+    check("C" == mySolution(inputTest))
+    mySolution(input).println() // wrong: KLCOMDGNNAAKBGGGCLEPHIBMOHBFJDGBBFADNMADABKDAFBMDPJEONAOHCIPGDIP
+}
+
+class Coordinate(
+    var i: Int,
+    var j: Int
+)
+
+enum class DirectionWithDeltas(
+    val iDelta: Int,
+    val jDelta: Int
+) {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+}
+
+private fun String.toDirectionWithDeltas(): DirectionWithDeltas {
+    return when (this) {
+        "UP" -> DirectionWithDeltas.UP
+        "DOWN" -> DirectionWithDeltas.DOWN
+        "LEFT" -> DirectionWithDeltas.LEFT
+        "RIGHT" -> DirectionWithDeltas.RIGHT
+        else -> throw RuntimeException("Error! Direction $this could not be parsed.")
+    }
+}
